817216b05fb62266a1ecad11fd14111e53c5c7f2
tausif104 <tausifahmed49@gmail.com>
2022-09-13 23:46:49 +0600

    homepage done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,49 @@
-const swiper = new Swiper('.swiper', {
-  slidesPerView: 1,
-  speed: 400,
-  navigation: {
-    nextEl: '.testimonial-next',
-    prevEl: '.testimonial-prev',
-  },
-})
+document.addEventListener('DOMContentLoaded', () => {
+  // swiper slider: testimonial
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    speed: 400,
+    loop: true,
+    navigation: {
+      nextEl: '.testimonial-next',
+      prevEl: '.testimonial-prev',
+    },
+  })
 
-swiper.slideNext()
+  swiper.slideNext()
+
+  // swiper slider: testimonial
+  const brandSlider = new Swiper('.brand-slider', {
+    slidesPerView: 5,
+    speed: 400,
+    loop: true,
+    breakpoints: {
+      200: {
+        slidesPerView: 1,
+      },
+
+      767: {
+        slidesPerView: 2,
+      },
+
+      1400: {
+        slidesPerView: 3,
+      },
+
+      1800: {
+        slidesPerView: 5,
+      },
+    },
+  })
+
+  brandSlider.slideNext()
+
+  // aos animation
+  AOS.init({
+    easing: 'ease-in-out',
+    anchorPlacement: 'center center',
+    duration: 700,
+    throttleDelay: 400,
+    once: true,
+  })
+})
